c972950c4a1e54ffa29af7a0efc03acdbd032b3b
Nick Prühs <dev@npruehs.de>
2014-04-24 18:34:40 +0200

    CHANGED: Config - Removed automatic generation of database evolutions. ADDED: Plugin - MySQL connector.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,6 +1,3 @@
-# --- Created by Ebean DDL
-# To stop Ebean DDL generation, remove this comment and start using Evolutions
-
 # --- !Ups
 
 create table game (
@@ -16,14 +13,14 @@ create table hearthstone_class (
 ;
 
 create table hearthstone_deck (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   deck_link                 varchar(255),
   hero_class_name           varchar(255),
   constraint pk_hearthstone_deck primary key (id))
 ;
 
 create table hearthstone_participation (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   participation_id          bigint,
   battle_tag                varchar(255),
   deck1_id                  bigint,
@@ -34,7 +31,7 @@ create table hearthstone_participation (
 ;
 
 create table match (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   round                     integer,
   tournament_id             bigint,
   player1_email             varchar(255),
@@ -45,14 +42,14 @@ create table match (
 ;
 
 create table participation (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   tournament_id             bigint,
   participant_email         varchar(255),
   constraint pk_participation primary key (id))
 ;
 
 create table tournament (
-  id                        bigint not null,
+  id                        bigint auto_increment not null,
   name                      varchar(255),
   date                      varchar(255),
   location                  varchar(255),
@@ -83,98 +80,62 @@ create table tournament_user (
   email                     varchar(255) not null,
   name                      varchar(255),
   password                  varchar(255),
-  is_admin                  boolean,
+  is_admin                  tinyint(1) default 0,
   constraint pk_tournament_user primary key (email))
 ;
 
-create sequence game_seq;
-
-create sequence hearthstone_class_seq;
-
-create sequence hearthstone_deck_seq;
-
-create sequence hearthstone_participation_seq;
-
-create sequence match_seq;
-
-create sequence participation_seq;
-
-create sequence tournament_seq;
-
-create sequence tournament_format_seq;
-
-create sequence tournament_mode_seq;
-
-create sequence tournament_user_seq;
-
-alter table hearthstone_deck add constraint fk_hearthstone_deck_heroClass_1 foreign key (hero_class_name) references hearthstone_class (name);
+alter table hearthstone_deck add constraint fk_hearthstone_deck_heroClass_1 foreign key (hero_class_name) references hearthstone_class (name) on delete restrict on update restrict;
 create index ix_hearthstone_deck_heroClass_1 on hearthstone_deck (hero_class_name);
-alter table hearthstone_participation add constraint fk_hearthstone_participation_p_2 foreign key (participation_id) references participation (id);
-create index ix_hearthstone_participation_p_2 on hearthstone_participation (participation_id);
-alter table hearthstone_participation add constraint fk_hearthstone_participation_d_3 foreign key (deck1_id) references hearthstone_deck (id);
-create index ix_hearthstone_participation_d_3 on hearthstone_participation (deck1_id);
-alter table hearthstone_participation add constraint fk_hearthstone_participation_d_4 foreign key (deck2_id) references hearthstone_deck (id);
-create index ix_hearthstone_participation_d_4 on hearthstone_participation (deck2_id);
-alter table hearthstone_participation add constraint fk_hearthstone_participation_d_5 foreign key (deck3_id) references hearthstone_deck (id);
-create index ix_hearthstone_participation_d_5 on hearthstone_participation (deck3_id);
-alter table match add constraint fk_match_tournament_6 foreign key (tournament_id) references tournament (id);
+alter table hearthstone_participation add constraint fk_hearthstone_participation_participation_2 foreign key (participation_id) references participation (id) on delete restrict on update restrict;
+create index ix_hearthstone_participation_participation_2 on hearthstone_participation (participation_id);
+alter table hearthstone_participation add constraint fk_hearthstone_participation_deck1_3 foreign key (deck1_id) references hearthstone_deck (id) on delete restrict on update restrict;
+create index ix_hearthstone_participation_deck1_3 on hearthstone_participation (deck1_id);
+alter table hearthstone_participation add constraint fk_hearthstone_participation_deck2_4 foreign key (deck2_id) references hearthstone_deck (id) on delete restrict on update restrict;
+create index ix_hearthstone_participation_deck2_4 on hearthstone_participation (deck2_id);
+alter table hearthstone_participation add constraint fk_hearthstone_participation_deck3_5 foreign key (deck3_id) references hearthstone_deck (id) on delete restrict on update restrict;
+create index ix_hearthstone_participation_deck3_5 on hearthstone_participation (deck3_id);
+alter table match add constraint fk_match_tournament_6 foreign key (tournament_id) references tournament (id) on delete restrict on update restrict;
 create index ix_match_tournament_6 on match (tournament_id);
-alter table match add constraint fk_match_player1_7 foreign key (player1_email) references tournament_user (email);
+alter table match add constraint fk_match_player1_7 foreign key (player1_email) references tournament_user (email) on delete restrict on update restrict;
 create index ix_match_player1_7 on match (player1_email);
-alter table match add constraint fk_match_player2_8 foreign key (player2_email) references tournament_user (email);
+alter table match add constraint fk_match_player2_8 foreign key (player2_email) references tournament_user (email) on delete restrict on update restrict;
 create index ix_match_player2_8 on match (player2_email);
-alter table participation add constraint fk_participation_tournament_9 foreign key (tournament_id) references tournament (id);
+alter table participation add constraint fk_participation_tournament_9 foreign key (tournament_id) references tournament (id) on delete restrict on update restrict;
 create index ix_participation_tournament_9 on participation (tournament_id);
-alter table participation add constraint fk_participation_participant_10 foreign key (participant_email) references tournament_user (email);
+alter table participation add constraint fk_participation_participant_10 foreign key (participant_email) references tournament_user (email) on delete restrict on update restrict;
 create index ix_participation_participant_10 on participation (participant_email);
-alter table tournament add constraint fk_tournament_game_11 foreign key (game_name) references game (name);
+alter table tournament add constraint fk_tournament_game_11 foreign key (game_name) references game (name) on delete restrict on update restrict;
 create index ix_tournament_game_11 on tournament (game_name);
-alter table tournament add constraint fk_tournament_format_12 foreign key (format_name) references tournament_format (name);
+alter table tournament add constraint fk_tournament_format_12 foreign key (format_name) references tournament_format (name) on delete restrict on update restrict;
 create index ix_tournament_format_12 on tournament (format_name);
-alter table tournament add constraint fk_tournament_mode_13 foreign key (mode_name) references tournament_mode (name);
+alter table tournament add constraint fk_tournament_mode_13 foreign key (mode_name) references tournament_mode (name) on delete restrict on update restrict;
 create index ix_tournament_mode_13 on tournament (mode_name);
 
 
 
 # --- !Downs
 
-drop table if exists game cascade;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists hearthstone_class cascade;
+drop table game;
 
-drop table if exists hearthstone_deck cascade;
+drop table hearthstone_class;
 
-drop table if exists hearthstone_participation cascade;
+drop table hearthstone_deck;
 
-drop table if exists match cascade;
+drop table hearthstone_participation;
 
-drop table if exists participation cascade;
+drop table match;
 
-drop table if exists tournament cascade;
+drop table participation;
 
-drop table if exists tournament_format cascade;
+drop table tournament;
 
-drop table if exists tournament_mode cascade;
+drop table tournament_format;
 
-drop table if exists tournament_user cascade;
+drop table tournament_mode;
 
-drop sequence if exists game_seq;
+drop table tournament_user;
 
-drop sequence if exists hearthstone_class_seq;
-
-drop sequence if exists hearthstone_deck_seq;
-
-drop sequence if exists hearthstone_participation_seq;
-
-drop sequence if exists match_seq;
-
-drop sequence if exists participation_seq;
-
-drop sequence if exists tournament_seq;
-
-drop sequence if exists tournament_format_seq;
-
-drop sequence if exists tournament_mode_seq;
-
-drop sequence if exists tournament_user_seq;
+SET FOREIGN_KEY_CHECKS=1;
 

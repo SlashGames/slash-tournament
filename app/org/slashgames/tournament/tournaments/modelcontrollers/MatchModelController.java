8bdd3d4e6855edeecc2472d6045ebaa0a9e27ce6
Nick Prühs <dev@npruehs.de>
2014-05-03 15:36:56 +0200

    CHANGED: View - Sorting of some form fields and table entries.

--- app/org/slashgames/tournament/tournaments/modelcontrollers/MatchModelController.java
+++ app/org/slashgames/tournament/tournaments/modelcontrollers/MatchModelController.java
@@ -29,7 +29,7 @@ public class MatchModelController {
 	}
 	
 	public static List<TournamentMatch> getMatches(Tournament tournament) {
-		return find.where().eq("tournament", tournament).findList();
+		return find.where().eq("tournament", tournament).orderBy().asc("round").orderBy().asc("player1.name").findList();
 	}
 	
 	public static List<TournamentMatch> getMatches(Tournament tournament, Integer round) {
